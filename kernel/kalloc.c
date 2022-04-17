814e6389d7492f0b40af88188c30c6a2b8c3df3a
jisen <az1125072149@outlook.com>
2022-04-17 14:30:14 +0800

    finish cpu per freelist

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -14,28 +14,35 @@ void freerange(void *pa_start, void *pa_end);
 extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
 
-struct run {
+struct run
+{
   struct run *next;
 };
 
-struct {
+struct freeMem
+{
   struct spinlock lock;
   struct run *freelist;
 } kmem;
 
-void
-kinit()
+struct freeMem cpu_free_list[NCPU];
+
+void kinit()
 {
-  initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
+  // initlock(&kmem.lock, "kmem");
+
+  for (int i = 0; i < NCPU; ++i)
+  {
+    initlock(&cpu_free_list[i].lock, "kmem");
+  }
+  freerange(end, (void *)PHYSTOP);
 }
 
-void
-freerange(void *pa_start, void *pa_end)
+void freerange(void *pa_start, void *pa_end)
 {
   char *p;
-  p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  p = (char *)PGROUNDUP((uint64)pa_start);
+  for (; p + PGSIZE <= (char *)pa_end; p += PGSIZE)
     kfree(p);
 }
 
@@ -43,23 +50,28 @@ freerange(void *pa_start, void *pa_end)
 // which normally should have been returned by a
 // call to kalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
-void
-kfree(void *pa)
+void kfree(void *pa)
 {
   struct run *r;
+  int cpu_id;
 
-  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+  if (((uint64)pa % PGSIZE) != 0 || (char *)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
+
+  // get cpu id
+  push_off();
+  cpu_id = cpuid();
+  pop_off();
 
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
 
-  r = (struct run*)pa;
+  r = (struct run *)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  acquire(&cpu_free_list[cpu_id].lock);
+  r->next = cpu_free_list[cpu_id].freelist;
+  cpu_free_list[cpu_id].freelist = r;
+  release(&cpu_free_list[cpu_id].lock);
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -69,14 +81,27 @@ void *
 kalloc(void)
 {
   struct run *r;
+  int cpu_id;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+  push_off();
+  cpu_id = cpuid();
+  pop_off();
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
-  return (void*)r;
+  // not found in this cpu free list
+  for (int i = 0; i < NCPU; i++, cpu_id = (cpu_id + 1) % NCPU)
+  {
+    acquire(&cpu_free_list[cpu_id].lock);
+    r = cpu_free_list[cpu_id].freelist;
+    if (r)
+      cpu_free_list[cpu_id].freelist = r->next;
+    release(&cpu_free_list[cpu_id].lock);
+
+    if (r)
+      break;
+    // found page, break
+  }
+
+  if (r)
+    memset((char *)r, 5, PGSIZE); // fill with junk
+  return (void *)r;
 }
